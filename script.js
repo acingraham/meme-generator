f4c4cf97755921c8a715bc42e88a23cfded63e09
Andrew Ingraham <acingraham@gmail.com>
2019-11-15 05:24:27 +0000

    Functional prototype

---
+++ script.js
@@ -0,0 +1,22 @@
+$("#download").click(function() {
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+    return document.body.appendChild(canvas);
+  }).then(() => {
+    var canvas = $("canvas")[0];
+    var image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+    var link = document.createElement('a');
+    link.download = "my-image.png";
+    link.href = image;
+    link.click();
+  });
+});
+
+$("#top-input").keydown(function() {
+  const val = $("#top-input").val();
+  $("#top-text").text(val);
+});
+
+$("#bottom-input").keydown(function() {
+  const val = $("#bottom-input").val();
+  $("#bottom-text").text(val);
+});
